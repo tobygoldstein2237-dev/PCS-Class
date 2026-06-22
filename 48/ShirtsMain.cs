10f4dc84f475bb98219cc886360fef1d8d67b1c6
tobygoldstein2237-dev <tgoldstein2237@gmail.com>
2026-06-22 09:50:21 -0400

    Initial commit

---
+++ 48/ShirtsMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW48 {
+    internal class ShirtsMain {
+
+        static void Main(string[] args) {
+            string[] Colors = { "red", "green", "blue" };
+            string[] Patterns = { "striped", "checked", "plain" };
+
+
+            foreach (string color in Colors) {
+                foreach (string pattern in Patterns) {
+                    Shirts shirt = new Shirts(color, pattern);
+                    Console.WriteLine(shirt);
+                }
+            }
+        }
+    }
+}
